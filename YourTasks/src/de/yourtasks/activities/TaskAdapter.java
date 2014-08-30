0f50b000879ea08f2dfbe1459d0a14d1542832bf
c2z1 <c2z1@web.de>
2014-08-30 22:21:14 +0200

    copy paste kommentare entfernt

--- YourTasks/src/de/yourtasks/activities/TaskAdapter.java
+++ YourTasks/src/de/yourtasks/activities/TaskAdapter.java
@@ -32,23 +32,18 @@ public abstract class TaskAdapter extends ArrayAdapter<Task> {
 	private Tasks service;
 	
 	private ActionMode.Callback actionModeCallback = new ActionMode.Callback() {
-	    // Called when the action mode is created; startActionMode() was called
 	    @Override
 	    public boolean onCreateActionMode(ActionMode mode, Menu menu) {
-	        // Inflate a menu resource providing context menu items
 	        MenuInflater inflater = mode.getMenuInflater();
 	        inflater.inflate(R.menu.task_list_context, menu);
 	        return true;
 	    }
 
-	    // Called each time the action mode is shown. Always called after onCreateActionMode, but
-	    // may be called multiple times if the mode is invalidated.
 	    @Override
 	    public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
-	        return false; // Return false if nothing is done
+	        return false; 
 	    }
 
-	    // Called when the user selects a contextual menu item
 	    @Override
 	    public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
 	        switch (item.getItemId()) {
@@ -61,7 +56,6 @@ public abstract class TaskAdapter extends ArrayAdapter<Task> {
 	        }
 	    }
 
-	    // Called when the user exits the action mode
 	    @Override
 	    public void onDestroyActionMode(ActionMode mode) {
 	        actionMode = null;
